c24aeaaaf25f3431d1f249c93dea31abeba9a36a
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-05-29 15:36:50 +0200

    Add functionality to download TestCreator if not present and integrate exam generation

--- src/main/python/programmingtheiot/cda/emulated/PICTestEmulatorTask.py
+++ src/main/python/programmingtheiot/cda/emulated/PICTestEmulatorTask.py
@@ -1,9 +1,22 @@
 import logging
-import webbrowser
 
 import programmingtheiot.common.ConfigConst as ConfigConst
 
 from programmingtheiot.cda.sim.BaseActuatorSimTask import BaseActuatorSimTask
+
+import subprocess
+import sys
+import os
+
+
+if not os.path.exists("TestCreator"):
+    logging.info("TestCreator directory not found. Running TestCreator script first.")
+    subprocess.run(["bash", "downloadTestCreator.sh"], check=True)
+
+sys.path.append(os.path.abspath("."))
+sys.path.append(os.path.abspath("TestCreator"))
+
+from TestCreator.UTILS import exam
 
 
 class PICTestEmulatorTask(BaseActuatorSimTask):
@@ -23,8 +36,18 @@ class PICTestEmulatorTask(BaseActuatorSimTask):
         self, val: float = ConfigConst.DEFAULT_VAL, stateData: str = None
     ) -> int:
 
-        # Temporary url
-        webbrowser.open("https://github.com/programming-the-iot/python-components")
+        # Change to the TestCreator directory
+        os.chdir(os.path.abspath("TestCreator"))
+
+        exam.examGenerator(
+            folderPath=os.path.abspath("PIC"),
+            numberOfExams=int(val),
+            numberOfQuestions=30,
+        )
+
+        # Change back to the original directory
+        os.chdir(os.path.abspath(".."))
+
         return 0
 
     def _deactivateActuator(
